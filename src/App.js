29b49822035dc82515205d2aa0a69d19819d75ac
Brian DiRito <bdirito91@gmail.com>
2021-04-06 19:11:11 -0400

    worked through useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import randomcolor from 'randomcolor'
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('')
 
   const clickHandler = () => {
     setCount(prevCount => prevCount + 1)
   }
 
+  useEffect(() => {
+    setColor(randomcolor())
+  }, [count])
+
   return (
     <div className="App">
-      <h1>{count}</h1>
+      <h1 style={{color: color}}>{count}</h1>
       <button onClick={clickHandler}>Change!</button>
     </div>
   );

32493416953060019d651ee7acd94aa0660830cf
Brian DiRito <bdirito91@gmail.com>
2021-04-08 19:00:35 -0400

    submit form via hooks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [color, setColor] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  
+
   const clickHandler = () => {
     setCount(prevCount => prevCount + 1)
   }
@@ -16,18 +16,27 @@ function App() {
     setColor(randomcolor())
   }, [count])
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const userInfoString = `${username} - ${password}`
+    document.getElementById("user-info").innerText = userInfoString
+  }
+
   return (
     <div className="App">
       <h1 style={{color: color}}>{count}</h1>
       <button onClick={clickHandler}>Change!</button>
 
-      <form>
+      <form onSubmit={e => submitHandler(e)}>
         <label>Username:</label>
-        <input type="text" name="username" value={username}/>
+        <input type="text" name="username" value={username} onChange={event => setUsername(event.target.value)}/>
         <br/>
         <label>Password:</label>
-        <input type="password" name="password" value={password} />
+        <input type="password" name="password" value={password} onChange={event => setPassword(event.target.value)}/>
+        <br/>
+        <input type="submit" />
       </form>
+      <span id="user-info"></span>
     </div>
   );
 }
